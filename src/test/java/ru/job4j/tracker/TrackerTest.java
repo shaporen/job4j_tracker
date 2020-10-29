ebab7cc686fe10f21225a864cc3535cd9231ed24
shaporen <shaporen@yandex.ru>
2020-10-29 14:23:39 +0300

    6. Метод замены заявки. Tracker.replace [#292266]

--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -14,4 +14,17 @@ public class TrackerTest {
         Item result = tracker.findById(item.getId());
         assertThat(result.getName(), is(item.getName()));
     }
+
+    @Test
+    public void whenReplace() {
+        Tracker tracker = new Tracker();
+        Item bug = new Item();
+        bug.setName("Bug");
+        tracker.add(bug);
+        int id = bug.getId();
+        Item bug2 = new Item();
+        bug2.setName("Bug with Description");
+        tracker.replace(id, bug2);
+        assertThat(tracker.findById(id).getName(), is("Bug with Description"));
+    }
 }
