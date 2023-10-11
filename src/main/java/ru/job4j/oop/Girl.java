6cdeb20e2d1656b2670592c706e74f2b7ee0ae0d
shaporen <shaporen@yandex.ru>
2023-10-11 21:54:39 +0300

    Сказка про Колобка

---
+++ src/main/java/ru/job4j/oop/Girl.java
@@ -0,0 +1,5 @@
+package ru.job4j.oop;
+
+public class Girl {
+    public void help(Pioneer pioneer) { }
+}
---
+++ src/main/java/ru/job4j/oop/Wolf.java
@@ -0,0 +1,5 @@
+package ru.job4j.oop;
+
+public class Wolf {
+    public void eat(Girl girl) { }
+}
