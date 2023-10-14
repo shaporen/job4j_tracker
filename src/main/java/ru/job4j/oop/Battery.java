fe43ab95c7274a231401bdd87d04604416522783
shaporen <shaporen@yandex.ru>
2023-10-14 19:48:20 +0300

    1.8. Класс использующий сам себя [#167388]

---
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -0,0 +1,25 @@
+package ru.job4j.oop;
+
+public class Battery {
+    public int load;
+
+    public Battery(int load) {
+        this.load = load;
+    }
+
+    public String about() {
+        return "My charge: " + load + "%";
+    }
+
+    public void exchange(Battery another) {
+        another.load = another.load + this.load;
+        this.load = 0;
+    }
+
+    public static void main(String[] args) {
+        Battery first = new Battery(30);
+        Battery second = new Battery(70);
+        System.out.println("First." + first.about());
+        System.out.println("Second." + second.about());
+    }
+}
