d72f05a408b6e657972007fe58ff8e320f655ee7
shaporen <shaporen@yandex.ru>
2023-10-21 14:24:52 +0300

    8. Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -8,10 +8,6 @@ public class Item {
 
     private LocalDateTime created = LocalDateTime.now();
 
-    public LocalDateTime getCreated() {
-        return created;
-    }
-
     public Item() { }
 
     public Item(String name) {
@@ -21,6 +17,10 @@ public class Item {
     public Item(String name, int id) {
         this.name = name;
         this.id = id;
+    }
+
+    public LocalDateTime getCreated() {
+        return created;
     }
 
     public int getId() {
