5c2a3af3521b483f68bf2fdc6cda0dd565d3ecb0
shaporen <shaporen@yandex.ru>
2023-10-21 16:36:31 +0300

    3. Массивы и модели. [#174892]

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -12,19 +12,22 @@ public class Library {
         books[2] = novel;
         books[3] = special;
         for (Book bks : books) {
-            System.out.println("Title: \"" + bks.getName() + "\". Number of pages: " + bks.getNumberOfPages());
+            System.out.println("Title: \"" + bks.getName() + "\". Number of pages: "
+                    + bks.getNumberOfPages());
         }
         Book tmp = books[3];
         books[3] = books[0];
         books[0] = tmp;
         System.out.println("\n");
         for (Book bks : books) {
-            System.out.println("Title: \"" + bks.getName() + "\". Number of pages: " + bks.getNumberOfPages());
+            System.out.println("Title: \"" + bks.getName() + "\". Number of pages: "
+                    + bks.getNumberOfPages());
         }
         System.out.println("\n");
         for (Book bks : books) {
-            if (bks.getName().equals("Clean code")){
-                System.out.println("Title: \"" + bks.getName() + "\". Number of pages: " + bks.getNumberOfPages());
+            if (bks.getName().equals("Clean code")) {
+                System.out.println("Title: \"" + bks.getName() + "\". Number of pages: "
+                        + bks.getNumberOfPages());
             }
         }
     }
