b2f5e06ffb39be1916ef2ef6fa4d60c9681886b5
shaporen <shaporen@yandex.ru>
2023-10-28 22:14:45 +0300

    10. toString [#310018]

--- src/main/java/ru/job4j/tracker/User.java
+++ src/main/java/ru/job4j/tracker/User.java
@@ -13,8 +13,8 @@ public class User {
 
     @Override
     public String toString() {
-        return "User{" +
-                "id=" + id
+        return "User{"
+                + "id=" + id
                 + ", username='" + username + '\''
                 + ", password='" + password + '\''
                 + '}';
