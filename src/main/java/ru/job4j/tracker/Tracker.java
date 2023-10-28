52764c367f2d62c198a27b458a98d966e44cc496
shaporen <shaporen@yandex.ru>
2023-10-28 15:14:06 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -21,5 +23,30 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findAll() {
+        Item[] rsl = new Item[items.length];
+        size = 0;
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] != null) {
+                rsl[size] = items[i];
+                size++;
+            }
+        }
+        return Arrays.copyOf(rsl, size);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] itemsCopy = findAll();
+        Item[] rsl = new Item[itemsCopy.length];
+        size = 0;
+        for (int i = 0; i < itemsCopy.length; i++) {
+            if (itemsCopy[i].getName().equals(key)) {
+                rsl[size] = items[i];
+                size++;
+            }
+        }
+        return Arrays.copyOf(rsl, size);
     }
 }
