9ef95fe2308115c0e2b12f8df6896a37610ffff7
shaporen <shaporen@yandex.ru>
2023-11-04 15:42:29 +0300

    4.2. Интерфейс Input [#181092]

--- src/main/java/ru/job4j/polymorph/MainFunc.java
+++ src/main/java/ru/job4j/polymorph/MainFunc.java
@@ -1,6 +1,6 @@
 package ru.job4j.polymorph;
 
-public class MainFunc implements Func1, Func2{
+public class MainFunc implements Func1, Func2 {
     @Override
     public double func(double x, double y) {
         return Func2.super.func(x, y);
