cf0de39768646e44d9900270c1c433d1777babe8
shaporen <shaporen@yandex.ru>
2023-11-04 16:16:24 +0300

    4.2. Интерфейс Input [#181092]

--- src/main/java/ru/job4j/poly/Transport.java
+++ src/main/java/ru/job4j/poly/Transport.java
@@ -2,12 +2,12 @@ package ru.job4j.poly;
 
 public interface Transport {
 
-    double price = 59.80;
+    double PRICE = 59.80;
 
     void drive();
     void passengers(int numberOfPass);
 
     default double refuel(double fuel) {
-        return price * fuel;
+        return PRICE * fuel;
     }
 }
