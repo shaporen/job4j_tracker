b9af7f2b16ac10024819f5ae33e698ae7fb330dd
shaporen <shaporen@yandex.ru>
2023-11-06 01:30:09 +0300

    1.1.6 - массивы - 3. Размер массива

---
+++ src/main/java/ru/job4j/exercises/array/ConcatArray.java
@@ -0,0 +1,21 @@
+package ru.job4j.exercises.array;
+
+public class ConcatArray {
+    public static int combineSize(int[] a, int[] b) {
+        int first = 0;
+        int second = 0;
+        for (int aa : a) {
+            first++;
+        }
+        for (int bb : b) {
+            second++;
+        }
+        return first + second;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {5, -2, 6};
+        int[] b = {5, -2, 6, 0};
+        System.out.println(combineSize(a,b));
+    }
+}
