3f37dbb90c06db52c33b3753480edf4fe0cb2007
shaporen <shaporen@yandex.ru>
2023-11-06 01:40:46 +0300

    1.1.6 - массивы - 12. Сумма элементов, пока не выполнится условие

---
+++ src/main/java/ru/job4j/exercises/array/SumWithStopEl.java
@@ -0,0 +1,16 @@
+package ru.job4j.exercises.array;
+
+public class SumWithStopEl {
+    public static int count(int[] data, int el) {
+        int cnt = 0;
+        for (int i = 0; data[i] < el; i++) {
+            cnt += data[i];
+        }
+        return cnt % 2 != 0? 0 : cnt;
+    }
+
+    public static void main(String[] args) {
+        int[] data = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println(count(data, 5));
+    }
+}
