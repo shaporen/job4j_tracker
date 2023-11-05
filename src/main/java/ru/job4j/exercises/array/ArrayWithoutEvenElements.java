b8c265e9031fd250dc05030905117c61f779d8fe
shaporen <shaporen@yandex.ru>
2023-11-06 02:15:48 +0300

    1.1.6 массивы - 5. Массив без четных значений

---
+++ src/main/java/ru/job4j/exercises/array/ArrayWithoutEvenElements.java
@@ -0,0 +1,24 @@
+package ru.job4j.exercises.array;
+
+import java.util.Arrays;
+
+public class ArrayWithoutEvenElements {
+    public static int[] changeData(int[] data) {
+        int size = 0;
+        int[] array = new int[data.length];
+        for (int a : data) {
+            if (a % 2 !=0) {
+                array[size++] = a;
+            }
+        }
+        return Arrays.copyOf(array, size);
+    }
+
+    public static void main(String[] args) {
+        int[] data = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] rsl = ArrayWithoutEvenElements.changeData(data);
+        for (int a : rsl) {
+            System.out.println(a);
+        }
+    }
+}
