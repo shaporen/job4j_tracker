d618495ee185ba88de53e12c64d481cd0f4fa991
shaporen <shaporen@yandex.ru>
2023-11-26 22:48:40 +0300

    Именование классов реализующих интерфейс [#505124]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,11 +1,8 @@
 package ru.job4j.tracker;
 
 import ru.job4j.tracker.action.*;
-import ru.job4j.tracker.input.ConsoleInput;
-import ru.job4j.tracker.input.Input;
-import ru.job4j.tracker.input.ValidateInput;
-import ru.job4j.tracker.output.ConsoleOutput;
-import ru.job4j.tracker.output.Output;
+import ru.job4j.tracker.input.*;
+import ru.job4j.tracker.output.*;
 
 public class StartUI {
     private final Output out;
