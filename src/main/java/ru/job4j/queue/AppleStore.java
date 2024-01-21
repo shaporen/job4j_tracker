2d8626a6cfc4b6712607e1ee5f5cf7f5fb2fb457
shaporen <shaporen@yandex.ru>
2024-01-21 21:36:10 +0300

    1. Интерфейс Queue [#504942]

---
+++ src/main/java/ru/job4j/queue/AppleStore.java
@@ -0,0 +1,27 @@
+package ru.job4j.queue;
+
+import java.util.*;
+
+public class AppleStore {
+    private final Queue<Customer> queue;
+    private final int count;
+
+    public AppleStore(Queue<Customer> queue, int count) {
+        this.queue = queue;
+        this.count = count;
+    }
+
+    public String getLastHappyCustomer() {
+        for (int i = 0; i < count - 1; i++) {
+            queue.poll();
+        }
+        return queue.poll().name();
+    }
+
+    public String getFirstUpsetCustomer() {
+        for (int i = 0; i < count; i++) {
+            queue.poll();
+        }
+        return queue.poll().name();
+    }
+}
---
+++ src/test/java/ru/job4j/queue/AppleStoreTest.java
@@ -0,0 +1,40 @@
+package ru.job4j.queue;
+
+import java.util.*;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class AppleStoreTest {
+    @Test
+    void whenGetLast() {
+        Queue<Customer> customers = new LinkedList<>();
+        customers.add(new Customer("Petr", 1000));
+        customers.add(new Customer("Stas", 1500));
+        customers.add(new Customer("Andrey", 850));
+        customers.add(new Customer("Alexei", 900));
+        customers.add(new Customer("Iryna", 1250));
+        customers.add(new Customer("Elena", 750));
+        customers.add(new Customer("Rail", 950));
+        int count = 4;
+        AppleStore appleStore = new AppleStore(customers, count);
+        String customer = appleStore.getLastHappyCustomer();
+        assertThat(customer).isEqualTo("Alexei");
+    }
+
+    @Test
+    void whenGetFirst() {
+        Queue<Customer> customers = new LinkedList<>();
+        customers.add(new Customer("Petr", 1000));
+        customers.add(new Customer("Stas", 1500));
+        customers.add(new Customer("Andrey", 850));
+        customers.add(new Customer("Alexei", 900));
+        customers.add(new Customer("Iryna", 1250));
+        customers.add(new Customer("Elena", 750));
+        customers.add(new Customer("Rail", 950));
+        int count = 4;
+        AppleStore appleStore = new AppleStore(customers, count);
+        String customer = appleStore.getFirstUpsetCustomer();
+        assertThat(customer).isEqualTo("Iryna");
+    }
+}
