54473292dcf7eab58bee9d2f7295758e664c1056
shaporen <shaporen@yandex.ru>
2024-01-22 21:46:59 +0300

    0. Map, HashMap [#212652]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,16 @@
+package ru.job4j.collection;
+
+import java.util.*;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("parsentev@yandex.ru", "Petr Arsentev");
+        map.put("shaporen@yandex.ru", "Sergey Shaporenko");
+        map.put("g.rudenko@yandex.ru", "Georgi Rudenko");
+        for (String key : map.keySet()) {
+            String value = map.get(key);
+            System.out.println(value);
+        }
+    }
+}
