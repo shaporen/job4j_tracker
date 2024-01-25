a9725536969ab80346502a71f2667b504aa932c5
shaporen <shaporen@yandex.ru>
2024-01-25 14:55:28 +0300

    1. Организовать сортировку User [#10034]

---
+++ src/main/java/ru/job4j/collection/User.java
@@ -0,0 +1,36 @@
+package ru.job4j.collection;
+
+import java.util.*;
+
+public class User implements Comparable<User> {
+    private String name;
+    private int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return age == user.age && Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+
+    @Override
+    public int compareTo(User user) {
+        int result = this.name.compareTo(user.name);
+        return result == 0 ? Integer.compare(this.age, user.age) : result;
+    }
+}
