8b862c2869bd99eb96f28c69aab252d4effb494f
shaporen <shaporen@yandex.ru>
2024-02-11 16:08:43 +0300

    3. Отсортировать департаменты [#34352]

--- src/main/java/ru/job4j/collection/Departments.java
+++ src/main/java/ru/job4j/collection/Departments.java
@@ -6,15 +6,10 @@ public class Departments {
     public static List<String> fillGaps(List<String> departments) {
         Set<String> temp = new LinkedHashSet<>();
         for (String value : departments) {
-            String[] firstDep = value.split("/");
-            String start = firstDep[0];
-            temp.add(start);
+            String start = "";
             for (String element : value.split("/")) {
-                if (!start.equals(element)) {
-                    String deps = String.join("/", start, element);
-                    temp.add(deps);
-                    start = deps;
-                }
+                start += "".equals(start) ? element : "/" + element;
+                temp.add(start);
             }
         }
         return new ArrayList<>(temp);
