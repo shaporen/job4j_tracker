edffc968b75b1f0849305dd0d1a341dce0ddc6e1
shaporen <shaporen@yandex.ru>
2024-02-18 18:17:48 +0300

    2.3. Встроенные функциональные интерфейсы. [#249209]

---
+++ src/main/java/ru/job4j/function/SearchFolder.java
@@ -0,0 +1,16 @@
+package ru.job4j.function;
+
+import java.util.*;
+import java.util.function.*;
+
+public class SearchFolder {
+    public static List<Folder> filter(List<Folder> list, Predicate<Folder> predicate) {
+        List<Folder> result = new ArrayList<>();
+        for (Folder folder : list) {
+            if (predicate.test(folder)) {
+                result.add(folder);
+            }
+        }
+        return result;
+    }
+}
