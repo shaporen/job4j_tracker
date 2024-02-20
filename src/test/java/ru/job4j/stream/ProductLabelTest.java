b621e4ff89c38a3b9c76183453258a9b161defe5
shaporen <shaporen@yandex.ru>
2024-02-20 22:39:36 +0300

    4. Перенос методов в Stream API [#279131]

--- src/test/java/ru/job4j/stream/ProductLabelTest.java
+++ src/test/java/ru/job4j/stream/ProductLabelTest.java
@@ -22,7 +22,8 @@ public class ProductLabelTest {
         );
         ProductLabel generator = new ProductLabel();
         List<String> strings = generator.generateLabels(products);
-        List<String> expected = Arrays.asList(one.toString(), two.toString(), three.toString(), four.toString());
+        List<String> expected = Arrays.asList(one.toString(),
+                two.toString(), three.toString(), four.toString());
         assertThat(strings).hasSameElementsAs(expected);
     }
 }
