48b972876dc460751aa30c3475877e251382fc5f
shaporen <shaporen@yandex.ru>
2024-03-09 13:47:31 +0300

    пример использования flatMap()

--- src/main/java/ru/job4j/stream/Phone.java
+++ src/main/java/ru/job4j/stream/Phone.java
@@ -6,17 +6,17 @@ public class Phone {
     private String name;
     private int price;
 
+    public Phone(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
     public String getName() {
         return name;
     }
 
     public int getPrice() {
         return price;
-    }
-
-    public Phone(String name, int price) {
-        this.name = name;
-        this.price = price;
     }
 
     public static void main(String[] args) {
